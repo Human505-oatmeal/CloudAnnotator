8449bfbdb144f16221b58f5369a8d7f5bb3cd4ff
Human505 <53027407+Human505-oatmeal@users.noreply.github.com>
2025-11-14 02:21:25 -0600

    Add empty lines for better readability in detect_labels

--- src/rekognition_client.py
+++ src/rekognition_client.py
@@ -1,5 +1,7 @@
 def detect_labels(bucket, key, session, max_labels=10, min_confidence=70):
     rekognition = session.client("rekognition", region_name="us-east-1")
+    
+    
     response = rekognition.detect_labels(
         Image={"S3Object": {"Bucket": bucket, "Name": key}},
         MaxLabels=max_labels,
